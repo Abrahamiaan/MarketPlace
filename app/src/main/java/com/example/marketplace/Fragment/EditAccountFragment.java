19ce93f5c3babe308493d4c1d7fc527519a19de6
vladabrahamian <vladabrahamyan96@gmail.com>
2024-02-27 02:23:11 +0400

    Implement account editing feature

---
+++ app/src/main/java/com/example/marketplace/Fragment/EditAccountFragment.java
@@ -0,0 +1,209 @@
+package com.example.marketplace.Fragment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.example.marketplace.R;
+import com.example.marketplace.databinding.FragmentEditAccountBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class EditAccountFragment extends Fragment {
+    private ActivityResultLauncher<Intent> imageSelectionLauncher;
+    FragmentEditAccountBinding binding;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    FirebaseFirestore db;
+    Uri onStartPhotoUri;
+    Uri selectedImageUri;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentEditAccountBinding.inflate(inflater, container, false);
+
+        initGlobalFields();
+        initListeners();
+
+        return binding.getRoot();
+    }
+
+    private void initGlobalFields() {
+        setupImageSelection();
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        onStartPhotoUri = currentUser.getPhotoUrl();
+
+
+        binding.userName.setText(currentUser.getDisplayName());
+        Glide.with(this)
+                .load(onStartPhotoUri)
+                .transform(new CircleCrop())
+                .into(new CustomTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                        binding.profilePhoto.setBackground(resource);
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {}
+                });
+    }
+    private void initListeners() {
+        binding.toBack.setOnClickListener(v -> requireActivity().onBackPressed());
+        binding.saveBtn.setOnClickListener(v -> {
+            checkChangesAndSave();
+            requireActivity().onBackPressed();
+        });
+        binding.sendResetPassword.setOnClickListener(v -> sendPasswordResetEmail());
+        binding.mainLinearProfessional.setOnClickListener(v -> {
+            binding.professionalDone.setVisibility(View.VISIBLE);
+            binding.individualDone.setVisibility(View.GONE);
+        });
+        binding.mainLinearIndividual.setOnClickListener(v -> {
+            binding.professionalDone.setVisibility(View.GONE);
+            binding.individualDone.setVisibility(View.VISIBLE);
+        });
+        binding.uploadProfilePhoto.setOnClickListener(v -> selectImage());
+    }
+    private void setupImageSelection() {
+        imageSelectionLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            requireActivity();
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                if (result.getData() != null) {
+                    selectedImageUri = result.getData().getData();
+                    Glide.with(this)
+                            .load(selectedImageUri)
+                            .transform(new CircleCrop())
+                            .into(new CustomTarget<Drawable>() {
+                                @Override
+                                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                                    binding.profilePhoto.setBackground(resource);
+                                }
+
+                                @Override
+                                public void onLoadCleared(@Nullable Drawable placeholder) {}
+                            });
+                }
+            }
+        });
+    }
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        imageSelectionLauncher.launch(intent);
+    }
+    private void checkChangesAndSave() {
+        checkAndSaveChanges();
+        boolean areNameChanges = checkNameChanges();
+        if (areNameChanges) {
+            changeDisplayName();
+        }
+
+        if (selectedImageUri != null) {
+            updateProfilePhoto(selectedImageUri);
+        }
+    }
+    private void changeDisplayName() {
+        String username = binding.userName.getText().toString();
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+                .build();
+
+        currentUser.updateProfile(profileUpdates)
+                .addOnCompleteListener(updateProfileTask -> {
+                    if (updateProfileTask.isSuccessful()) {
+                        Log.d("Edit Account: ", "Update Profile Successfully");
+                    } else {
+                        Exception updateProfileException = updateProfileTask.getException();
+                        Log.e("Update Profile Failed", Objects.requireNonNull(updateProfileException).getMessage());
+                    }
+                });
+    }
+    private void updateProfilePhoto(Uri photoUri) {
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(photoUri)
+                .build();
+
+        currentUser.updateProfile(profileUpdates)
+                .addOnCompleteListener(updateProfileTask -> {
+                    if (updateProfileTask.isSuccessful()) {
+                        Log.d("Edit Account: ", "Update Profile Photo Successfully");
+                    } else {
+                        Exception updateProfileException = updateProfileTask.getException();
+                        Log.e("Update Profile Photo Failed", Objects.requireNonNull(updateProfileException).getMessage());
+                    }
+                });
+    }
+    private void sendPasswordResetEmail() {
+        mAuth.sendPasswordResetEmail(currentUser.getEmail())
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(requireActivity(), R.string.password_reset_email_sent_successfully, Toast.LENGTH_SHORT).show();
+                        Log.d("Edit Account: ", "Password Reset Email Sent Successfully");
+                    } else {
+                        Log.e("Edit Account: ", "Error Sending Password Reset Email", task.getException());
+                    }
+                });
+    }
+    private void checkAndSaveChanges() {
+        DocumentReference docRef = db.collection("DeliveryAddresses").document(currentUser.getUid());
+        String newAddress = binding.deliveryAddress.getText().toString();
+
+        docRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    String storedAddress = document.getString("address");
+                    if (storedAddress != null && !storedAddress.equals(newAddress)) {
+                        updateAddressInFirebase(newAddress);
+                    }
+                }
+            } else {
+                Exception e = task.getException();
+                Log.e("Edit Account: ", Arrays.toString(e.getStackTrace()));
+            }
+        });
+    }
+    private void updateAddressInFirebase(String newAddress) {
+        DocumentReference docRef = db.collection("DeliveryAddresses").document(currentUser.getUid());
+        Map<String, Object> addressData = new HashMap<>();
+        addressData.put("address", newAddress);
+
+        docRef.update(addressData)
+                .addOnSuccessListener(aVoid -> Log.d("Edit Account", "Address updated successfully"))
+                .addOnFailureListener(e -> Log.e("Edit Account", "Error updating address", e));
+    }
+    private boolean checkNameChanges() {
+        return !(binding.userName.getText().toString().equals(currentUser.getDisplayName()));
+    }
+}
--- app/src/main/java/com/example/marketplace/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/marketplace/Fragment/ProfileFragment.java
@@ -1,8 +1,12 @@
 package com.example.marketplace.Fragment;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,10 +15,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.marketplace.Activity.SplashActivity;
 import com.example.marketplace.R;
 import com.example.marketplace.databinding.FragmentProfileBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 import ir.androidexception.andexalertdialog.AndExAlertDialog;
@@ -22,10 +31,12 @@ import ir.androidexception.andexalertdialog.AndExAlertDialog;
 public class ProfileFragment extends Fragment {
     FragmentProfileBinding binding;
     FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+
 
     public ProfileFragment() { }
     @Override
-    public View onCreateView(LayoutInflater inflater,
+    public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
@@ -55,16 +66,35 @@ public class ProfileFragment extends Fragment {
                 })
                 .build();
     }
-    private void initListeners() {
-        mAuth = FirebaseAuth.getInstance();
-        String currentUserName = mAuth.getCurrentUser().getDisplayName();
-
-        binding.tvName.setText(currentUserName);
-    }
     private void initGlobalFields() {
-        binding.linearOut.setOnClickListener(v -> {
-            showConfirmDialog(getString(R.string.oops), getString(R.string.are_you_sure_you_want_to_log_out));
-        });
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String currentUserName = currentUser.getDisplayName();
+            Uri photoUrl = currentUser.getPhotoUrl();
+
+            binding.tvName.setText(currentUserName);
+
+            if (photoUrl != null) {
+                System.out.println(photoUrl);
+                Glide.with(this)
+                        .load(photoUrl)
+                        .transform(new CircleCrop())
+                        .into(new CustomTarget<Drawable>() {
+                            @Override
+                            public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                                binding.profilePhoto.setBackground(resource);
+                            }
+
+                            @Override
+                            public void onLoadCleared(@Nullable Drawable placeholder) {}
+                        });
+            }
+        }
+    }
+
+    private void initListeners() {
+        binding.linearOut.setOnClickListener(v -> showConfirmDialog(getString(R.string.oops), getString(R.string.are_you_sure_you_want_to_log_out)));
         binding.linearLanguage.setOnClickListener(v -> {
             LanguageFragment languageFragment = new LanguageFragment();
 
@@ -76,14 +106,13 @@ public class ProfileFragment extends Fragment {
 
             transaction.commit();
         });
-
-        binding.linearFavorites.setOnClickListener(v -> {
-            FavoriteFragment languageFragment = new FavoriteFragment();
+        binding.constraintEditAccount.setOnClickListener(v -> {
+            EditAccountFragment fragment = new EditAccountFragment();
 
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-            transaction.replace(R.id.frame_layout, languageFragment);
+            transaction.replace(R.id.frame_layout, fragment);
             transaction.addToBackStack(null);
 
             transaction.commit();
